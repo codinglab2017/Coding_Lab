1cc25883d9705fc1c2e0ba0adabc8a1425a6f90b
till <tf.langbein@goethe.finance>
2017-05-11 16:32:35 +0200

    Die ersten Übungen!

--- Utils.py
+++ Utils.py
@@ -1,24 +1,65 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue May  9 16:00:24 2017
 
-@author: Till Langbein
-"""
+#############################################################################
+# By the way: Zeilen die mit einem Hashtag beginnen und Text zwischen je drei 
+# Gänsefüßchen ist ein Kommentar und wird nicht als Programm ausgeführt.
+# Er dient nur dazu, mit anderen Programmierern im Code zu kommunizieren und
+# ihnen klarzumachen warum man etwas so gemacht hat.
+# Je größer die Teams werden, desto wichtiger ist eine umfangreiche 
+# Kommentierung.
+#############################################################################
 
-def sharpe_ratio(rp:float, rf:float, sdp:float)->float:
+def sharpe_ratio(rp, rf, sdp):
     """
+    rp : rendite des Portfolios
+    rf : risikofreier Zins
+    sdp : Volatilität des Portfolios
+    
     rp-rf
-    ------
+    geteilt durch:
     sdp
     """
     pass
 
-def standart_deviation(p:list)->float:
+
+def share_dividend_value(rf, div0, g):
     """
-    1) Nimm den Durchschnitt deiner Liste
-    2) Zieh von jedem Punkt den Durchschnitt ab
-    3) Quadriere jeden Punkt
-    4) Teile die Summe aller solcher Punkte durch die Anzahl der Punkte -1
+    Es muss gelten: g < rf
+    
+    rf : risikofreier Zinssatz
+    div0 : Anfängliche Dividende
+    g : Dividendenwachstum pro Periode
+    
+    div0 
+    mal
+    (1 + g)
+    mal
+    (rf - g)^-1
+    """
+    pass
+
+
+def total_stock_return(p0, p1, D):
+    """
+    p0 : Einstiegspreis
+    p1 : Endpreis
+    D : Dividendenzahlungen
+    
+    (p1 - p0) + D
+    geteilt durch
+    p0
+    """
+    pass
+
+
+def standard_deviation(p):
+    """
+    p : eine Liste [] von Werten
+    
+    1) Errechne den Durchschnittswert deiner Liste
+    2) Zieh von jedem Element der Liste den Durchschnitt ab
+    3) Quadriere jedes Element
+    4) Teile die Summe aller solcher Elemente durch die Anzahl der Elemente -1
     5) Zieh nun die Wurzel aus dem Ergebnis
 
     Hinweise:
@@ -28,20 +69,22 @@ def standart_deviation(p:list)->float:
     """
     pass
 
-
-
-
-
-
-
-
-
-
-
+############################################################################
+# Bitte fügt weitere Funktionen hinzu, die ihr gerne in einem Börsenspiel
+# zur Verfügung hättet!
+############################################################################
 
 if __name__ == "__main__":
-    assert sharpe_ratio(5, 4, 10) == 0.1, "Your sharpe_ratio should return 0.1"
-    assert standart_deviation([1, 2, 3, 4, 5, 4, 5, 6, 5, 6, 7, 4, 5, 8, 7, 9])\
-            == 5.0625, "Your standart_deviation should return 5.0625"
+    # Dieser Teil wird ausgeführt, wenn ihr das Programm ausführt
+    # Es wird ein Fehler geworfen, wenn eine Funktion ein 
+    # falsches Ergebnis liefert
+    assert sharpe_ratio(5, 4, 10) == 0.1, \
+            "Your sharpe_ratio(5, 4, 10) should return 0.1"
+    assert share_dividend_value(0.05, 10, 0) == 200.0, \
+            "Your share_dividend_value(0.05, 10, 0) should return 200"
+    assert total_stock_return(100, 110, 10) == 0.2, \
+            "Your total_stock_return(100, 110, 10) should return 0.2"
+    assert standard_deviation([1, 2, 3, 4, 5, 4, 5, 6, 5, 6, 7, 4, 5, 8, 7, 9]) == 5.0625, \
+            "Your standard_deviation([1, 2, 3, 4, 5, 4, 5, 6, 5, 6, 7, 4, 5, 8, 7, 9]) should return 5.0625"
     
 
